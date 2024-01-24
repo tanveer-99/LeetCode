ea42f650022ba1caf17950c2f5c7e189eb193e27
TAN <55363306+tanveer-99@users.noreply.github.com>
2024-01-24 14:42:54 +0600

    Create (EASY) BS - 367.Valid Perfect Square.java

---
+++ (EASY) BS - 367.Valid Perfect Square.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 0;
+        int right = num;
+        int mid = 0;
+        boolean ans = false;
+        while(left<=right) {
+            mid = left + (right-left)/2;
+            if((long)mid*mid==num) {
+                ans = true;
+                break;
+            }
+            else {
+                if((long)mid*mid>num) {
+                    right = mid-1;
+                }
+                else {
+                    left = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
